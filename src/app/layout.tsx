3c3fb5863aad2646403724e53dc832d2956b7d1c
aarevalom0 <a.arevalom@uniandes.edu.co>
2025-09-12 09:11:17 -0500

    prepare for chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Newsreader   } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Chatbot from "@/components/Chatbot";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${newsreader.variable}antialiased`}
       >
         <NavBar/>
+        <Chatbot />
         {children}
       </body>
     </html>
