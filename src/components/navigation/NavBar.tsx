20483aa167942dfe33971df3dffc33ac87297a43
Gabs-489 <gabrielazambranoz@hotmail.com>
2025-11-30 20:55:20 -0500

    Main page casi completo

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -20,7 +20,7 @@ const NavBar = () => {
   const switchLocale = (newLocale: string) => {
     router.replace(pathname, { locale: newLocale });
   };
-  
+
   const [menuOpen, setMenuOpen] = useState(false);
   const [selected, setSelected] = useState("Home");
 
@@ -44,6 +44,7 @@ const NavBar = () => {
             <li title={t('navbar.stats')} ><Link href="/estadisticas" className={linkCls("Estadísticas")} onClick={() => setSelected("Estadísticas")}>{t('navbar.stats')} </Link></li>
             <li title={t('navbar.library')} ><Link href="/miBiblioteca" className={linkCls("Mi Biblioteca")} onClick={() => setSelected("Mi Biblioteca")}>{t('navbar.library')} </Link></li>
             <li title={t('navbar.readNow')} ><Link href="/leerAhora" className={linkCls("Leer Ahora")} onClick={() => setSelected("Leer Ahora")}>{t('navbar.readNow')} </Link></li>
+            <li title={t('navbar.books')} ><Link href="/libros" className={linkCls("Libros")} onClick={() => setSelected("Libros")}>{t('navbar.books')} </Link></li>
             <li title={t('navbar.community')} ><Link href="/comunidad" className={linkCls("Comunidad")} onClick={() => setSelected("Comunidad")}>{t('navbar.community')} </Link></li>
           </ul>
         </div>
@@ -52,31 +53,24 @@ const NavBar = () => {
         <div className="hidden md:flex items-center gap-3">
 
           {/* notificaciones */}
-          <button title={t('navbar.notifications')}  className="cursor-pointer p-2 rounded-lg border border-elev bg-[var(--input-bg)] hover:opacity-80">
+          <button title={t('navbar.notifications')} className="cursor-pointer p-2 rounded-lg border border-elev bg-[var(--input-bg)] hover:opacity-80">
             <NotificationsIcon className="text-[var(--text)]/70" />
           </button>
 
-          {/* perfil */}
-          <Link href="/perfil" className="flex items-center">
-            <div title={t('navbar.profile')}  className="w-10 h-10 rounded-full overflow-hidden border border-elev">
-              <Image src="/Images/Perfil1.png" alt="Perfil" width={40} height={40} />
-            </div>
-          </Link>
 
-          <div title={t('navbar.language')}  className="flex gap-2">
+          <div title={t('navbar.language')} className="flex gap-2">
             {AVAILABLE_LOCALES.map((l) => (
               <button
                 key={l}
                 onClick={() => switchLocale(l)}
-                className= {`cursor-pointer px-3 py-1 rounded ${
-                  locale === l ? "bg-[var(--colorMenus)] text-white" : "bg-gray-200"
-                }`}
+                className={`cursor-pointer px-3 py-1 rounded ${locale === l ? "bg-[var(--colorMenus)] text-white" : "bg-gray-200"
+                  }`}
               >
                 {l.toUpperCase()}
               </button>
             ))}
           </div>
-          
+
         </div>
 
         {/* Botón menú móvil */}
@@ -110,6 +104,7 @@ const NavBar = () => {
             <li><Link href="/estadisticas" className={linkCls("Estadísticas")} onClick={() => setSelected("Estadísticas")}>Estadísticas</Link></li>
             <li><Link href="/miBiblioteca" className={linkCls("Mi Biblioteca")} onClick={() => setSelected("Mi Biblioteca")}>Mi biblioteca</Link></li>
             <li><Link href="/leerAhora" className={linkCls("Leer Ahora")} onClick={() => setSelected("Leer Ahora")}>Leer Ahora</Link></li>
+            <li><Link href="/libros" className={linkCls("Libros")} onClick={() => setSelected("Libros")}>Libros</Link></li>
             <li><Link href="/comunidad" className={linkCls("Comunidad")} onClick={() => setSelected("Comunidad")}>Comunidad</Link></li>
           </ul>
         </div>
